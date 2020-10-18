31e1663384646481a17b971384cf1f1472dc2574
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-18 12:35:04 +0600

    Linked List Data structure

---
+++ linked_list.js
@@ -0,0 +1,10 @@
+// A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations
+
+// Three Types of Linked List: 
+
+// 1) Singly Linked List
+// 2) Doubly Linked List
+// 3) Circular Linked List
+
+// See The Code Example below :
+
