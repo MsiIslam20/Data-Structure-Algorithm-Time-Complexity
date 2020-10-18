d61930fe54c0413b6ec18cfb3a21f197e3015f3f
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-18 12:01:36 +0600

    stack done

--- stack.js
+++ stack.js
@@ -1,3 +1,5 @@
+// Stack Data Structure:
+
 // Last In First Out
 // LIFO
 
@@ -5,5 +7,24 @@ class Stack {
     constructor() {
        this.stack = []; 
     }
-    
+    add(item) {
+        this.stack.push(item);
+    }
+    remove(){
+        if(this.stack.length){
+            return this.stack.pop();
+        }
+    }   
 }
+
+const guest = new Stack();
+guest.add("Saidul Islam");
+guest.add("Shakil Ahmed");
+guest.add("Sadekur Rahman");
+guest.add("Abdul Hasib");
+// console.log(guest.stack);
+
+const speaker = guest.remove();
+
+// console.log(speaker);
+// console.log(guest.stack);
