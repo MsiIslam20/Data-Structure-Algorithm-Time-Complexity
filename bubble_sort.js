61532f5948111728dde2cc31e66f40721dd6ca26
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-18 17:00:57 +0600

    sorting algorithm

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,3 +1,6 @@
+//Refer Link:
+//https://medium.com/javascript-in-plain-english/simple-sorting-algorithms-in-javascript-57d512ceaf5d
+
 const numbers = [10, 20, 23, 2, 8, 70, 22, 33, 6, 55, 97];
 
 function bubbleSort(array) {
@@ -13,5 +16,20 @@ function bubbleSort(array) {
     return array;
 }
 
+function bubbleSortDesc(array){
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if(array[j] < array[j+1]){
+                const temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
 const sorted = bubbleSort(numbers);
-console.log(sorted);
+const sortedDes = bubbleSortDesc(numbers);
+// console.log(sorted);
+// console.log(sortedDes);
