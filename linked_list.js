69cd5b45555678e25933b48e1143d071d7972c07
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-18 12:39:00 +0600

    linked list data structure

--- linked_list.js
+++ linked_list.js
@@ -8,3 +8,46 @@
 
 // See The Code Example below :
 
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+class LinkedList {
+    constructor(head){
+        this.head = head;
+    }
+    add(node){
+        const newNode = new Node(node);
+        let current = this.head;
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    remove(value) {
+        let current = this.head;
+        let previous = null;
+        while (current !== null) {
+            if(current.value === value){
+                previous.next = current.next;
+                break;
+            }
+            previous = current;
+            current = current.next;
+        }
+    }
+}
+
+const head = new Node("Saidul Islam");
+
+const manobBondonList = new LinkedList(head);
+
+manobBondonList.add("Shakil");
+manobBondonList.add("Aminul");
+manobBondonList.add("Maruf");
+
+// console.log(JSON.stringify(manobBondonList));
