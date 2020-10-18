fd6ba01c425b5c7fbc2f68ff6a7426bcae8be1df
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-18 12:14:12 +0600

    Dictionary & Hash Data done

---
+++ dictionary.js
@@ -0,0 +1,25 @@
+// Dictionary And Hash Table
+
+class Dictionary {
+    constructor() {
+        this.dictionary = {};
+    }
+    add(key , value){
+        this.dictionary[key] = value;
+    }
+    get(key){
+        return this.dictionary[key];
+    }
+}
+
+const phoneBook = new Dictionary();
+
+phoneBook.add("saidul", "01755555555")
+phoneBook.add("Shakil", "01888888555")
+phoneBook.add("Aminul", "01999999999")
+
+// console.log(phoneBook.dictionary);
+
+const aminul = phoneBook.get("Aminul");
+
+// console.log(aminul);
